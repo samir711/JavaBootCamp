8c3151f78c158b9ae30836e753f31523d2273db8
samir711 <samirdoc1@gmail.com>
2018-05-16 22:59:29 -0400

    Adding new files

---
+++ JavaExercise/University/Progs/CompareTwoNumbers/src/CompareTwoNums.java
@@ -0,0 +1,19 @@
+public class CompareTwoNums {
+
+    public static void main(String[] args) {
+
+        //Declare 2 variables
+
+        int numberOne = 265;
+        int numberTwo = 365;
+
+        if(numberOne > numberTwo) {
+            System.out.println(numberOne + " is greater than " + numberTwo);
+        } else if(numberOne < numberTwo) {
+            System.out.println(numberOne + " is less than " + numberTwo);
+        } else{
+            System.out.println(numberOne + " is equal to  " + numberTwo);
+        }
+
+    }
+}
